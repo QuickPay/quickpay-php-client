a12651295c45b0d112cb588afe9bd1ce3dca6b64
Wel Rachid <wel@rachid.dk>
2022-11-03 14:23:45 +0100

    Enable dynamic endpoint
    Invoicing endpoint enabled. Will autoinject header

--- QuickPay/API/Constants.php
+++ QuickPay/API/Constants.php
@@ -4,6 +4,7 @@ namespace QuickPay\API;
 
 class Constants
 {
-    public const API_URL     = 'https://api.quickpay.net/';
-    public const API_VERSION = '10';
+    public const API_URL                = 'https://api.quickpay.net/';
+    public const INVOICING_API_URL      = 'https://invoicing.quickpay.net/';
+    public const API_VERSION            = '10';
 }
